a1f8e641f7fe9f719c75416b3c480e6c044f7172
uma_j <uma.jayanthi@photoninfotech.net>
2013-08-26 15:52:48 +0530

    TestNg DataProvider with seleniumGrid

---
+++ src/test/java/com/photon/phresco/testcases/WelcomePageTestCase.java
@@ -0,0 +1,237 @@
+/**
+ * PHR_HTML5YUIWidget
+ *
+ * Copyright (C) 1999-2013 Photon Infotech Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.photon.phresco.testcases;
+
+import java.io.IOException;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.photon.phresco.Screens.WelcomeScreen;
+import com.photon.phresco.model.YuiWidgetsData.YuiWidget;
+import com.photon.phresco.uiconstants.PhrescoUiConstants;
+import com.photon.phresco.uiconstants.UIConstants;
+
+public class WelcomePageTestCase {
+
+	private  UIConstants uiConstants;
+	private  PhrescoUiConstants phrescoUIConstants;
+	private  WelcomeScreen welcomeScreen;
+	private  String methodName;
+	private  String selectedBrowser;
+	
+	
+
+	@Parameters(value = { "browser", "platform" })
+	@BeforeTest
+	public  void setUp(String browser, String platform) throws Exception {
+		try {
+			phrescoUIConstants = new PhrescoUiConstants();
+			uiConstants = new UIConstants();
+		
+			String selectedBrowser = browser;
+			String selectedPlatform = platform;
+			
+			methodName = Thread.currentThread().getStackTrace()[1]
+					.getMethodName();
+			String applicationURL = phrescoUIConstants.getProtocol() + "://"
+					+ phrescoUIConstants.getHost() + ":" + phrescoUIConstants.getPort()
+					+ "/";
+			welcomeScreen = new WelcomeScreen(selectedBrowser, selectedPlatform, applicationURL,
+					phrescoUIConstants.getContext(),  uiConstants);
+			Thread.sleep(5000);
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testWelcomePageScreen() throws InterruptedException,
+			IOException, Exception {
+		try {
+		System.out
+					.println("---------testWelcomePageScreen-------------");
+			Assert.assertNotNull(welcomeScreen);
+			 Thread.sleep(1000);
+		} catch (Exception t) {
+			t.printStackTrace();
+
+		}
+	}
+
+	@Test(dataProvider = "widgetData", dataProviderClass=com.photon.phresco.uiconstants.TestDataProvider.class)
+	public void testToVerifyTheAudioDevicesAddToCart(YuiWidget yuiWidget)
+			throws InterruptedException, IOException, Exception {
+		try {
+
+			System.out
+					.println("---------testToVerifyTheAudioDevicesAddToCart()-------------");
+			welcomeScreen.AudioDevices(methodName);
+			welcomeScreen.billingInfo(methodName,yuiWidget);
+		} catch (Exception t) {
+			t.printStackTrace();
+
+		}
+	}
+
+	@Test(dataProvider = "widgetData", dataProviderClass=com.photon.phresco.uiconstants.TestDataProvider.class)
+	public void testToVerifyTheCamerasAddToCart(YuiWidget yuiWidget) throws InterruptedException,
+			IOException, Exception {
+		try {
+
+			System.out
+					.println("---------testToVerifyTheCamerasAddToCart()-------------");
+			welcomeScreen.Cameras(methodName);
+			welcomeScreen.billingInfo(methodName,yuiWidget);
+		} catch (Exception t) {
+			t.printStackTrace();
+
+		}
+	}
+
+	@Test(dataProvider = "widgetData", dataProviderClass=com.photon.phresco.uiconstants.TestDataProvider.class)
+	public void testToVerifyTheVideoGamesAddToCart(YuiWidget yuiWidget)
+			throws InterruptedException, IOException, Exception {
+		try {
+
+			System.out
+					.println("---------testToVerifyTheVideoGamesAddToCart()-------------");
+			welcomeScreen.VideoGames(methodName);
+			welcomeScreen.billingInfo(methodName,yuiWidget);
+		} catch (Exception t) {
+			t.printStackTrace();
+
+		}
+	}
+
+	@Test(dataProvider = "widgetData", dataProviderClass=com.photon.phresco.uiconstants.TestDataProvider.class)
+	public void testToVerifyTheTelevisionAddToCart(YuiWidget yuiWidget)
+			throws InterruptedException, IOException, Exception {
+		try {
+
+			System.out
+					.println("---------testToVerifyTheTelevisionAddToCart()-------------");
+			welcomeScreen.Television(methodName);
+			welcomeScreen.billingInfo(methodName,yuiWidget);
+		} catch (Exception t) {
+			t.printStackTrace();
+
+		}
+	}
+
+	@Test(dataProvider = "widgetData", dataProviderClass=com.photon.phresco.uiconstants.TestDataProvider.class)
+	public void testToVerifyTheTabletsAddToCart(YuiWidget yuiWidget) throws InterruptedException,
+			IOException, Exception {
+		try {
+
+			System.out
+					.println("---------testToVerifyTheTabletsAddToCart()-------------");
+			welcomeScreen.Tablets(methodName);
+			welcomeScreen.billingInfo(methodName,yuiWidget);
+		} catch (Exception t) {
+			t.printStackTrace();
+
+		}
+	}
+
+	@Test(dataProvider = "widgetData", dataProviderClass=com.photon.phresco.uiconstants.TestDataProvider.class)
+	public void testToVerifyTheMP3PlayersAddToCart(YuiWidget yuiWidget)
+			throws InterruptedException, IOException, Exception {
+		try {
+
+			System.out
+					.println("---------testToVerifyTheMP3PlayersAddToCart()-------------");
+			welcomeScreen.MP3Players(methodName);
+			welcomeScreen.billingInfo(methodName,yuiWidget);
+		} catch (Exception t) {
+			t.printStackTrace();
+
+		}
+	}
+
+	@Test(dataProvider = "widgetData", dataProviderClass=com.photon.phresco.uiconstants.TestDataProvider.class)
+	public void testToVerifyTheMoviesAndMusicAddToCart(YuiWidget yuiWidget)
+			throws InterruptedException, IOException, Exception {
+		try {
+
+			System.out
+					.println("---------testToVerifyTheMoviesAndMusicAddToCart()-------------");
+			welcomeScreen.MoviesnMusic(methodName);
+			welcomeScreen.billingInfo(methodName,yuiWidget);
+		} catch (Exception t) {
+			t.printStackTrace();
+
+		}
+	}
+
+	@Test(dataProvider = "widgetData", dataProviderClass=com.photon.phresco.uiconstants.TestDataProvider.class)
+	public void testToVerifyTheMobilePhonesAddToCart(YuiWidget yuiWidget)
+			throws InterruptedException, IOException, Exception {
+		try {
+
+			System.out
+					.println("---------testToVerifyTheMobilePhonesAddToCart()-------------");
+			welcomeScreen.MobilePhones(methodName);
+			welcomeScreen.billingInfo(methodName,yuiWidget);
+		} catch (Exception t) {
+			t.printStackTrace();
+
+		}
+	}
+
+	@Test(dataProvider = "widgetData", dataProviderClass=com.photon.phresco.uiconstants.TestDataProvider.class)
+     public void testToVerifyTheAccessoriesAddToCart(YuiWidget yuiWidget)
+		throws InterruptedException, IOException, Exception {
+	try {
+		System.out
+				.println("---------testToVerifyTheAccessoriesAddToCart()-------------");
+		welcomeScreen.Accessories(methodName);
+		welcomeScreen.billingInfo(methodName,yuiWidget);
+	} catch (Exception t) {
+			t.printStackTrace();
+
+	}
+	}
+
+	@Test(dataProvider = "widgetData", dataProviderClass=com.photon.phresco.uiconstants.TestDataProvider.class)
+	public void testToVerifyTheComputersAddToCart(YuiWidget yuiWidget)
+			throws InterruptedException, IOException, Exception {
+		try {
+			System.out
+					.println("---------testToVerifyTheComputersAddToCart()-------------");
+			welcomeScreen.Computers(methodName);
+			welcomeScreen.billingInfo(methodName,yuiWidget);
+		} catch (Exception t) {
+			t.printStackTrace();
+
+		}
+	} 
+	
+	
+
+	@AfterTest
+	public  void tearDown() {
+		welcomeScreen.closeBrowser();
+	}
+
+	
+	
+}
